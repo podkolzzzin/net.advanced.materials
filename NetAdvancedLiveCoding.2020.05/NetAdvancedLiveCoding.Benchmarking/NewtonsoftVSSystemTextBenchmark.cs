d26f536e98e3e10eab0a0df88c9cf587fa0c8808
Podkolzin <podkolzzzin@yandex.ru>
2021-01-25 14:55:11 +0200

    Add files via upload

---
+++ NetAdvancedLiveCoding.2020.05/NetAdvancedLiveCoding.Benchmarking/NewtonsoftVSSystemTextBenchmark.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace Benchmarking
+{
+    public class NewtonsoftVSSystemTextBenchmark
+    {
+
+        private const string PATH = @"C:\Users\andrii.podkolzin\source\repos\Benchmarking\Benchmarking\json\";
+
+        [Benchmark]
+        public void SystemTextJson()
+        {
+            var files = Directory.EnumerateFiles(@$"{PATH}\*.json");
+            foreach(var f in files)
+            {
+                var text = File.ReadAllText(f);
+                JsonDocument.Parse(text);
+            }
+        }
+
+        [Benchmark]
+        public void NewtonsoftJson()
+        {
+            foreach(var file in new DirectoryInfo(@$"{PATH}").EnumerateFiles())
+            {
+                using (var stream = file.OpenText())
+                {
+                    using (var reader = new JsonTextReader(stream))
+                    {
+                        JToken.Load(reader);
+                    }
+                }
+            }
+        }
+    }
+}
