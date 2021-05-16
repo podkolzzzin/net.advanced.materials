05f6991298701031266e3d1147eb2e0cb66cc33b
Podkolzin <podkolzzzin@yandex.ru>
2021-05-16 12:40:32 +0300

    Add files via upload

---
+++ NetAdvancedLiveCoding.Generators/NetAdvancedLiveCoding.Generators/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace NetAdvancedLiveCoding.Generators
+{
+    [Generator]
+    public class MySourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+                
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+           
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
