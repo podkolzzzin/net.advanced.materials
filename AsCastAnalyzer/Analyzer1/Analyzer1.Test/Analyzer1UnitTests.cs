4cc6fb109b2a548bea2b6612fac21d77c2937372
Podkolzin <podkolzzzin@yandex.ru>
2021-05-16 12:19:49 +0300

    Add files via upload

---
+++ AsCastAnalyzer/Analyzer1/Analyzer1.Test/Analyzer1UnitTests.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using VerifyCS = Analyzer1.Test.CSharpCodeFixVerifier<
+    Analyzer1.AsCastAnalyzer,
+    Analyzer1.Analyzer1CodeFixProvider>;
+
+namespace Analyzer1.Test
+{
+    interface IService { }
+
+    [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    sealed class MyAttribute : System.Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+
+        // This is a positional argument
+        public MyAttribute()
+        {
+            this.positionalString = positionalString;
+
+        }
+
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+
+        // This is a named argument
+        public int NamedInt { get; set; }
+    }
+
+    [My]
+    class Service
+    {
+
+    }
+
+    [TestClass]
+    public class Analyzer1UnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"
+namespace Analyzer1.Test
+{
+    interface IService { }
+
+    [System.AttributeUsage(System.AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    sealed class MyAttribute : System.Attribute
+    {
+        public MyAttribute()
+        {
+        }
+    }
+
+    [My]
+    class Service : IService
+    {
+
+    }
+
+    [My]
+    class Service2 : IService { }
+}";
+
+            await CSharpAnalyzerVerifier<Analyzer1Analyzer>.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task AsWithNoNullCheck()
+        {
+            string test = @"
+using System;
+
+class Wrong 
+{
+    class Nested {
+        public string Value {get;set;}
+    }
+    public int M(object x) {
+
+        var str = x as string;
+        str = x as string;
+        return str.Length;
+    }
+
+    public int M2(object x) {
+        
+        var o = new Nested();
+        o.Value = x as string;
+        return o.Value.Length;
+    }
+
+    public int M3(object x) {
+
+        var str = x as string;
+        if (str == null)
+            return str.Length;
+        return 0;
+    }
+}
+";
+            await CSharpAnalyzerVerifier<AsCastAnalyzer>.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("Analyzer1").WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
---
+++ AsCastAnalyzer/Analyzer1/Analyzer1/AsCastAnalyzer.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace Analyzer1
+{
+    class Wrong
+    {
+        public int M(object x)
+        {
+
+            string str;
+            str = x as string;
+            return str.Length;
+        }
+
+        public int M3(object x)
+        {
+
+            var str = x as string;
+            if (str == null)
+                return str.Length;
+            return 0;
+        }
+    }
+
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AsCastAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AsCastAnalyzer";
+        internal static readonly LocalizableString Title = "AsCastAnalyzer Title";
+        internal static readonly LocalizableString MessageFormat = "AsCastAnalyzer '{0}'";
+        internal const string Category = "AsCastAnalyzer Category";
+
+        private HashSet<ISymbol> asAsignedSymbols = new HashSet<ISymbol>();
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, 
+                SyntaxKind.SimpleAssignmentExpression, SyntaxKind.VariableDeclarator);
+            context.RegisterSyntaxNodeAction(AnalyzeAccess, SyntaxKind.SimpleMemberAccessExpression);
+        }
+
+        private void AnalyzeAccess(SyntaxNodeAnalysisContext obj)
+        {
+            var symbol = obj.SemanticModel.GetSymbolInfo(obj.Node);
+            MemberAccessExpressionSyntax node = obj.Node as MemberAccessExpressionSyntax;
+            CheckSymbol(obj, symbol);
+            CheckSymbol(obj, obj.SemanticModel.GetSymbolInfo(node.Expression));
+        }
+
+        private void CheckSymbol(SyntaxNodeAnalysisContext obj, SymbolInfo symbol)
+        {
+            if (asAsignedSymbols.Contains(symbol.Symbol))
+            {
+                var diagnostic = Diagnostic.Create(Rule, obj.Node.GetLocation(), symbol.Symbol.Name);
+                obj.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext obj)
+        {
+            if (obj.Node is VariableDeclaratorSyntax vds)
+            {
+                if (vds.Initializer.Value.IsKind(SyntaxKind.AsExpression))
+                {
+                    var candidate = obj.SemanticModel.GetDeclaredSymbol(vds);
+                    asAsignedSymbols.Add(candidate);
+                }
+            }
+            else if (obj.Node is AssignmentExpressionSyntax aes)
+            {
+                if (aes.Right.IsKind(SyntaxKind.AsExpression))
+                {
+                    var candidate = obj.SemanticModel.GetSymbolInfo(aes.Left);
+                    asAsignedSymbols.Add(candidate.Symbol);
+                }
+            }
+        }
+    }
+}
