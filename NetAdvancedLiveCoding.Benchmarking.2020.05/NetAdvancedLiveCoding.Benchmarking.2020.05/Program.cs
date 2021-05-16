d74a60eb2e2941009a858efdffe35d669f0eab16
Podkolzin <podkolzzzin@yandex.ru>
2021-05-16 12:38:43 +0300

    Add files via upload

---
+++ NetAdvancedLiveCoding.Benchmarking.2020.05/NetAdvancedLiveCoding.Benchmarking.2020.05/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Threading;
+
+namespace NetAdvancedLiveCoding.Benchmarking._2020._05
+{
+    public class ThreadSleepBenchmark
+    {
+        [Params(5, 50)]
+        public int Delay =5;
+
+        [Benchmark]
+        public void ThreadSleep()
+        {
+            Thread.Sleep(Delay);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<ThreadSleepBenchmark>();
+        }
+    }
+}
