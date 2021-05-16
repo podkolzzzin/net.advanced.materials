06c7c23574e1512bd56f763e0c01e1f3960a1621
Podkolzin <podkolzzzin@yandex.ru>
2021-05-16 12:43:24 +0300

    Add files via upload

---
+++ NetUnderTheHood2021.1/NetUnderTheHood2021.1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NetUnderTheHood2021._1
+{
+    struct SampleStruct
+    {
+        public byte B;
+        public ushort S;
+    }
+
+    struct SampleAlignment
+    {
+        public byte B1;
+        public byte B2;
+        public ulong L;
+        public uint I;
+
+    }
+
+    class SampleCast
+    {
+        public uint A, B, C, D;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sArr = new uint[] { 0xAAAAAAAA, 0xBBBBBBBB, 0xCCCCCCCC, 0xDDDDDDDD };
+            var bArr = ChangeType(sArr, new SampleCast());
+            var bPtr = AddressOf(bArr);
+        }
+
+        private static unsafe SampleCast ChangeType(uint[] sArr, SampleCast sampleCast)
+        {
+            var arrPtr = (long*)AddressOf(sArr).ToPointer();
+            var dstPtr = (long*)AddressOf(sampleCast).ToPointer();
+            *arrPtr = *dstPtr;
+            return (SampleCast)(object)sArr;
+        }
+
+        private static unsafe byte[] ChangeType(uint[] sArr, byte[] v)
+        {
+            var arrPtr = (long*)AddressOf(sArr).ToPointer();
+            var dstPtr = (long*)AddressOf(v).ToPointer();
+            long newLength = sizeof(uint) / sizeof(byte) * sArr.LongLength;
+            *arrPtr = *dstPtr;
+            *(arrPtr + 1) = newLength;
+            return (byte[])(object)sArr;
+        }
+
+        private static unsafe IntPtr AddressOf(object str)
+        {
+            TypedReference tr = __makeref(str);
+            return **(IntPtr**)&tr;
+        }
+    }
+}
